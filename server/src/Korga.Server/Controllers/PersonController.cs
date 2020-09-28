d2ae1a452c787ee6bfbcf1a883087cbbad27708b
Daniel Lerch <daniel.p.lerch@gmail.com>
2020-09-28 12:53:55 +0200

    Implement person controller

---
+++ server/src/Korga.Server/Controllers/PersonController.cs
@@ -0,0 +1,46 @@
+﻿using Korga.Server.Database;
+using Korga.Server.Database.Entities;
+using Korga.Server.Models.Json;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+using DbPerson = Korga.Server.Database.Entities.Person;
+using Person = Korga.Server.Models.Json.Person;
+
+namespace Korga.Server.Controllers
+{
+    [ApiController]
+    public class PersonController : ControllerBase
+    {
+        private readonly DatabaseContext database;
+
+        public PersonController(DatabaseContext database)
+        {
+            this.database = database;
+        }
+
+        [HttpGet("~/api/people")]
+        public async Task<IActionResult> Index([FromQuery] int offset = 0, [FromQuery] int count = 50)
+        {
+            var people = await database.People
+                .Select(p => new Person(p.Id, p.GivenName, p.FamilyName, p.MailAddress))
+                .Skip(offset).Take(count).ToListAsync();
+
+            return new JsonResult(people);
+        }
+
+        [HttpGet("~/api/person/{id}")]
+        public async Task<IActionResult> GetPerson(int id)
+        {
+            DbPerson? person = await database.People.Where(p => p.Id == id).Include(p => p.Creator).Include(p => p.Deletor).SingleOrDefaultAsync();
+            if (person == null) return StatusCode(404);
+
+            List<PersonSnapshot> snapshots = await database.PersonSnapshots.Where(ps => ps.PersonId == id).Include(ps => ps.Editor).ToListAsync();
+
+            return new JsonResult(new Person2(person, snapshots));
+        }
+    }
+}
---
+++ server/src/Korga.Server/Models/Json/Person.cs
@@ -0,0 +1,75 @@
+﻿using Korga.Server.Database.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+using DbPerson = Korga.Server.Database.Entities.Person;
+
+namespace Korga.Server.Models.Json
+{
+    public class Person
+    {
+        public Person(int id, string givenName, string familyName, string? mailAddress)
+        {
+            Id = id;
+            GivenName = givenName;
+            FamilyName = familyName;
+            MailAddress = mailAddress;
+        }
+
+        public Person(DbPerson person) : this(person.Id, person.GivenName, person.FamilyName, person.MailAddress) { }
+
+        public int Id { get; set; }
+        public string GivenName { get; set; }
+        public string FamilyName { get; set; }
+        public string? MailAddress { get; set; }
+    }
+
+    public class Person2
+    {
+        public Person2(DbPerson person, IList<PersonSnapshot> history)
+        {
+            Id = person.Id;
+            Version = person.Version;
+            GivenName = person.GivenName;
+            FamilyName = person.FamilyName;
+            MailAddress = person.MailAddress;
+            CreationTime = person.CreationTime;
+            Creator = person.Creator == null ? null : new Person(person.Creator);
+            DeletionTime = person.DeletionTime == default ? null : (DateTime?)person.DeletionTime;
+            Deletor = person.Deletor == null ? null : new Person(person.Deletor);
+            History = new List<Snapshot>(history.Select(ps => new Snapshot(ps)));
+        }
+
+        public int Id { get; set; }
+        public int Version { get; }
+        public string GivenName { get; set; }
+        public string FamilyName { get; set; }
+        public string? MailAddress { get; set; }
+        public DateTime CreationTime { get; set; }
+        public Person? Creator { get; set; }
+        public DateTime? DeletionTime { get; set; }
+        public Person? Deletor { get; set; }
+        public IList<Snapshot> History { get;set; }
+
+        public class Snapshot
+        {
+            public Snapshot(PersonSnapshot snapshot)
+            {
+                Version = snapshot.Version;
+                GivenName = snapshot.GivenName;
+                FamilyName = snapshot.FamilyName;
+                MailAddress = snapshot.MailAddress;
+                EditTime = snapshot.EditTime;
+                Editor = snapshot.Editor == null ? null : new Person(snapshot.Editor);
+            }
+
+            public int Version { get; set; }
+            public string GivenName { get; set; }
+            public string FamilyName { get; set; }
+            public string? MailAddress { get; set; }
+            public DateTime EditTime { get; set; }
+            public Person? Editor { get; set; }
+        }
+    }
+}
