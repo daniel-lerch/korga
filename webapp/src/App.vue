deb179b16307a745c50c38b1bf978c8e9baa6fec
Daniel Lerch <daniel.p.lerch@gmail.com>
2020-09-29 15:37:41 +0200

    Import Bootstrap from CDN and implement navbar

--- webapp/src/App.vue
+++ webapp/src/App.vue
@@ -1,31 +1,34 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link> |
-    <router-link to="/people">People</router-link>
+  <div>
+    <header>
+      <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <router-link to="/" class="navbar-brand">Korga</router-link>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul class="navbar-nav">
+            <li class="nav-item">
+              <router-link to="/" class="nav-link">Home</router-link>
+            </li>
+            <li class="nav-item">
+              <router-link to="/about" class="nav-link">About</router-link>
+            </li>
+            <li class="nav-item">
+              <router-link to="/people" class="nav-link">People</router-link>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    </header>
+    <router-view />
   </div>
-  <router-view/>
 </template>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
+/* Apply Bootstrap active link design to active router-link */
+.navbar-light .navbar-nav a.router-link-exact-active {
+  color: rgba(0,0,0,.9)!important;
 }
 </style>
