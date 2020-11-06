e9fbd1718cee3f4595f1628fe94c8af47f4cd56c
Daniel Lerch <daniel.p.lerch@gmail.com>
2020-11-06 16:55:27 +0100

    Implement dynamic base path for routing and API access

--- webapp/src/services/client.ts
+++ webapp/src/services/client.ts
@@ -1,5 +1,7 @@
+const baseUrl = process.env.VUE_APP_API_URL ?? window.resourceBasePath.slice(0, -1)
+
 export async function get<T> (query: string): Promise<T> {
-  const response = await fetch('http://localhost:50805' + query)
+  const response = await fetch(baseUrl + query)
   if (response.ok === false) {
     throw new Error('Unexpected status code ' + response.status)
   }
@@ -7,7 +9,7 @@ export async function get<T> (query: string): Promise<T> {
 }
 
 export async function post<T> (query: string, body: object): Promise<T> {
-  const response = await fetch('http://localhost:50805' + query, {
+  const response = await fetch(baseUrl + query, {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json'
---
+++ webapp/src/window.d.ts
@@ -0,0 +1,6 @@
+interface Window {
+  /**
+   * Gets the dynamic base path setting which is injected in index.html
+   */
+  resourceBasePath: string;
+}
