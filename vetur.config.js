683f4fb05116bfb9a3a0265274dbab389d43b853
Daniel Lerch <daniel.p.lerch@gmail.com>
2020-12-23 19:06:18 +0100

    Add Vetur extension config file and update NPM packages

---
+++ vetur.config.js
@@ -0,0 +1,6 @@
+// This file is required for the VS Code Vetur extension
+// in order to find the frontend project in its subfolder.
+/** @type {import('vls').VeturConfig} */
+module.exports = {
+  projects: [ './webapp' ]
+}
