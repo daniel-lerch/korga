2b9845593a0765a5ff4729b150e16f9de70c217b
Beniox <37512887+Beniox@users.noreply.github.com>
2022-03-14 22:09:43 +0100

    disable register button if all programs are full

--- webapp/src/views/EventList.vue
+++ webapp/src/views/EventList.vue
@@ -22,10 +22,7 @@
           :to="{ name: 'Register', params: { id: event.id } }"
           class="btn btn-primary"
           :class="{
-            disabled:
-              event.programs.length < 1 ||
-              (event.programs.length == 1 &&
-                event.programs[0].count >= event.programs[0].limit),
+            disabled: full(event),
           }"
           >Anmelden</router-link
         >
@@ -47,7 +44,15 @@ export default defineComponent({
       // console.log(events.value)
     });
 
-    return { events };
+    const full = function (event: EventResponse) {
+      if (event === null) return false;
+      for (const program of event.programs) {
+        if (program.count < program.limit) return false;
+      }
+      return true;
+    };
+
+    return { events, full };
   },
 });
 </script>
