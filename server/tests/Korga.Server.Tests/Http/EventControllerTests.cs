267f87af80e4563c8757dd7199af2b021373526e
Daniel Lerch <36048059+daniel-lerch@users.noreply.github.com>
2022-03-26 16:54:40 +0100

    Add more unit tests for EventController

--- server/tests/Korga.Server.Tests/Http/EventControllerTests.cs
+++ server/tests/Korga.Server.Tests/Http/EventControllerTests.cs
@@ -19,8 +19,9 @@ public class EventControllerTests : IDisposable
     private readonly HttpClient client;
     private readonly IServiceScope serviceScope;
     private readonly DatabaseContext database;
-    private readonly long eventId;
-    private readonly long programId;
+
+    private readonly Event testEvent;
+    private readonly EventProgram testProgram;
 
     public EventControllerTests()
     {
@@ -29,32 +30,69 @@ public class EventControllerTests : IDisposable
         serviceScope = server.Services.CreateScope();
         database = serviceScope.ServiceProvider.GetRequiredService<DatabaseContext>();
 
-        var @event = new Event(nameof(EventControllerTests));
-        database.Events.Add(@event);
+        testEvent = new Event(Guid.NewGuid().ToString());
+        database.Events.Add(testEvent);
         database.SaveChanges();
-        eventId = @event.Id;
 
-        var program = new EventProgram(nameof(EventControllerTests)) { EventId = eventId, Limit = 2 };
-        database.EventPrograms.Add(program);
+        testProgram = new EventProgram(Guid.NewGuid().ToString())
+        {
+            EventId = testEvent.Id,
+            Limit = 2
+        };
+        database.EventPrograms.Add(testProgram);
         database.SaveChanges();
-        programId = program.Id;
     }
 
     public void Dispose()
     {
-        Event? @event = database.Events.SingleOrDefault(e => e.Id == eventId);
-        if (@event is not null)
-        {
-            database.Events.Remove(@event);
-            database.SaveChanges();
-        }
+        database.Events.Remove(testEvent);
+        database.SaveChanges();
         serviceScope.Dispose();
+        server.Dispose();
+        client.Dispose();
     }
 
     [Fact]
-    public async Task TestRegister()
+    public async Task TestGetEvents_Empty()
     {
-        var request = new EventRegistrationRequest("Max", "Mustermann") { ProgramId = programId };
+        var events = await client.GetFromJsonAsync<EventResponse[]>("/api/events");
+        Assert.NotNull(events);
+        var response = events.Single(e => e.Id == testEvent.Id);
+        Assert.Equal(testEvent.Name, response.Name);
+        var program = response.Programs.Single();
+        Assert.Equal(testProgram.Id, program.Id);
+        Assert.Equal(testProgram.Name, program.Name);
+        Assert.Equal(testProgram.Limit, program.Limit);
+        Assert.Equal(0, program.Count);
+    }
+
+    [Fact]
+    public async Task TestGetEvent_Full()
+    {
+        var participants = new EventParticipant[]
+        {
+            new("Katharina", "Schäfer") { ProgramId = testProgram.Id },
+            new("Anna", "Schäfer") { ProgramId = testProgram.Id }
+        };
+        database.EventParticipants.AddRange(participants);
+        await database.SaveChangesAsync();
+
+        var response = await client.GetFromJsonAsync<EventResponse2>($"/api/event/{testEvent.Id}");
+        Assert.NotNull(response);
+        Assert.Equal(testEvent.Name, response.Name);
+        var program = response.Programs.Single();
+        Assert.Equal(testProgram.Id, program.Id);
+        Assert.Equal(testProgram.Name, program.Name);
+        Assert.Equal(testProgram.Limit, program.Limit);
+        Assert.Equal(2, program.Participants.Count);
+        Assert.NotEqual(0, program.Participants.Single(p => p.GivenName == "Katharina" && p.FamilyName == "Schäfer").Id);
+        Assert.NotEqual(0, program.Participants.Single(p => p.GivenName == "Anna" && p.FamilyName == "Schäfer").Id);
+    }
+
+    [Fact]
+    public async Task TestRegister_Single()
+    {
+        var request = new EventRegistrationRequest("Max", "Mustermann") { ProgramId = testProgram.Id };
         var response = await client.PostAsJsonAsync("/api/events/register", request);
         Assert.Equal(HttpStatusCode.NoContent, response.StatusCode);
     }
@@ -62,15 +100,15 @@ public class EventControllerTests : IDisposable
     [Fact]
     public async Task TestRegister_TooMany()
     {
-        EventRegistrationRequest request1 = new("Johannes", "Schäfer") { ProgramId = programId };
+        EventRegistrationRequest request1 = new("Johannes", "Schäfer") { ProgramId = testProgram.Id };
         var response1 = await client.PostAsJsonAsync("/api/events/register", request1);
         Assert.Equal(HttpStatusCode.NoContent, response1.StatusCode);
 
-        EventRegistrationRequest request2 = new("Katharina", "Schäfer") { ProgramId = programId };
+        EventRegistrationRequest request2 = new("Katharina", "Schäfer") { ProgramId = testProgram.Id };
         var response2 = await client.PostAsJsonAsync("/api/events/register", request2);
         Assert.Equal(HttpStatusCode.NoContent, response2.StatusCode);
 
-        EventRegistrationRequest request3 = new("Anna", "Schäfer") { ProgramId = programId };
+        EventRegistrationRequest request3 = new("Anna", "Schäfer") { ProgramId = testProgram.Id };
         var response3 = await client.PostAsJsonAsync("/api/events/register", request3);
         Assert.Equal(HttpStatusCode.Conflict, response3.StatusCode);
     }
