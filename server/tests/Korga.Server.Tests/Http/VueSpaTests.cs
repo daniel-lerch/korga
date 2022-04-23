7e8183207efda26e227ff2aa58c280c40e02592f
Daniel Lerch <36048059+daniel-lerch@users.noreply.github.com>
2022-04-23 22:34:50 +0200

    Improve VueSpaTests code style

--- server/tests/Korga.Server.Tests/Http/VueSpaTests.cs
+++ server/tests/Korga.Server.Tests/Http/VueSpaTests.cs
@@ -8,28 +8,18 @@ namespace Korga.Server.Tests.Http;
 
 public class VueSpaTests
 {
-    [Fact]
-    public async Task TestDefault()
+    [Theory]
+    [InlineData("", "/")]
+    [InlineData("", "/events/list")]
+    [InlineData("/korga/test/pathbase", "/")]
+    [InlineData("/korga/test/pathbase", "/events/list")]
+    public async Task TestVueEntrypoint(string pathbase, string url)
     {
-        TestServer server = TestHost.CreateTestServer(new Dictionary<string, string?> { { "Hosting:PathBase", null } });
-        HttpClient client = server.CreateClient();
-
-        var response = await client.GetAsync("/");
-        Assert.Equal("text/html", response.Content.Headers.ContentType?.MediaType);
-        string body = await response.Content.ReadAsStringAsync();
-        Assert.True(body.StartsWith("<!DOCTYPE html>"), "Body is missing DOCTYPE");
-        Assert.True(body.Contains("window.resourceBasePath = '/'"), "Body is missing pathbase");
-    }
-
-    [Fact]
-    public async Task TestPathbase()
-    {
-        const string pathbase = "/korga/test/pathbase";
-
         TestServer server = TestHost.CreateTestServer(new Dictionary<string, string?> { { "Hosting:PathBase", pathbase } });
         HttpClient client = server.CreateClient();
 
-        var response = await client.GetAsync(pathbase);
+        var response = await client.GetAsync(pathbase + url);
+
         Assert.Equal("text/html", response.Content.Headers.ContentType?.MediaType);
         string body = await response.Content.ReadAsStringAsync();
         Assert.True(body.StartsWith("<!DOCTYPE html>"), "Body is missing DOCTYPE");
