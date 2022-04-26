c4127db0d212db092401754634ba7cb750f8a297
Daniel Lerch <36048059+daniel-lerch@users.noreply.github.com>
2022-04-26 23:35:57 +0200

    Dynamically choose EF Core provider

--- server/src/Korga.Server/Database/DatabaseContext.cs
+++ server/src/Korga.Server/Database/DatabaseContext.cs
@@ -1,50 +1,29 @@
-﻿using Korga.Server.Configuration;
-using Korga.Server.Database.Entities;
+﻿using Korga.Server.Database.Entities;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.EntityFrameworkCore.Metadata.Builders;
-using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.Options;
 
-namespace Korga.Server.Database
+namespace Korga.Server.Database;
+
+public sealed class DatabaseContext : DbContext
 {
-    public sealed partial class DatabaseContext : DbContext
+    public DbSet<Event> Events => Set<Event>();
+    public DbSet<EventProgram> EventPrograms => Set<EventProgram>();
+    public DbSet<EventParticipant> EventParticipants => Set<EventParticipant>();
+
+    public DatabaseContext(DbContextOptions<DatabaseContext> options) : base(options) { }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
-        private const string currentTimestamp = "CURRENT_TIMESTAMP(6)";
-        private readonly IOptions<DatabaseOptions> options;
-        private readonly ILoggerFactory loggerFactory;
+        base.OnModelCreating(modelBuilder);
 
-        public DbSet<Event> Events => Set<Event>();
-        public DbSet<EventProgram> EventPrograms => Set<EventProgram>();
-        public DbSet<EventParticipant> EventParticipants => Set<EventParticipant>();
+        var @event = modelBuilder.Entity<Event>();
+        @event.HasKey(e => e.Id);
 
-        public DatabaseContext(IOptions<DatabaseOptions> options, ILoggerFactory loggerFactory)
-        {
-            this.options = options;
-            this.loggerFactory = loggerFactory;
-        }
+        var program = modelBuilder.Entity<EventProgram>();
+        program.HasKey(p => p.Id);
+        program.HasOne(p => p.Event).WithMany().HasForeignKey(p => p.EventId);
 
-        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-        {
-            base.OnConfiguring(optionsBuilder);
-
-            optionsBuilder.UseLoggerFactory(loggerFactory);
-            optionsBuilder.UseMySql(options.Value.ConnectionString, ServerVersion.AutoDetect(options.Value.ConnectionString));
-        }
-
-        protected override void OnModelCreating(ModelBuilder modelBuilder)
-        {
-            base.OnModelCreating(modelBuilder);
-
-            var @event = modelBuilder.Entity<Event>();
-            @event.HasKey(e => e.Id);
-
-            var program = modelBuilder.Entity<EventProgram>();
-            program.HasKey(p => p.Id);
-            program.HasOne(p => p.Event).WithMany().HasForeignKey(p => p.EventId);
-
-            var participant = modelBuilder.Entity<EventParticipant>();
-            participant.HasKey(p => p.Id);
-            participant.HasOne(p => p.Program).WithMany(p => p.Participants).HasForeignKey(p => p.ProgramId);
-        }
+        var participant = modelBuilder.Entity<EventParticipant>();
+        participant.HasKey(p => p.Id);
+        participant.HasOne(p => p.Program).WithMany(p => p.Participants).HasForeignKey(p => p.ProgramId);
     }
 }
--- server/src/Korga.Server/Program.cs
+++ server/src/Korga.Server/Program.cs
@@ -1,5 +1,4 @@
 using Korga.Server.Commands;
-using Korga.Server.Database;
 using Korga.Server.Extensions;
 using Korga.Server.Services;
 using Korga.Server.Utilities;
@@ -12,66 +11,65 @@ using System;
 using System.Diagnostics;
 using System.Threading.Tasks;
 
-namespace Korga.Server
+namespace Korga.Server;
+
+public class Program
 {
-    public class Program
+    public static async Task<int> Main(string[] args)
     {
-        public static async Task<int> Main(string[] args)
-        {
 #if DEBUG
-            if (Debugger.IsAttached && !NativeMethods.IsRunningInProcessIIS())
-            {
-                Console.Write("Korga server is running in debug mode. Please enter your command: ");
-                args = Console.ReadLine()?.Split(' ', StringSplitOptions.RemoveEmptyEntries) ?? Array.Empty<string>();
-                Console.WriteLine();
-            }
+        if (Debugger.IsAttached && !NativeMethods.IsRunningInProcessIIS())
+        {
+            Console.Write("Korga server is running in debug mode. Please enter your command: ");
+            args = Console.ReadLine()?.Split(' ', StringSplitOptions.RemoveEmptyEntries) ?? Array.Empty<string>();
+            Console.WriteLine();
+        }
 #endif
-            if (args.Length == 0)
+        if (args.Length == 0)
+        {
+            await CreateWebHostBuilder(args).Build().RunAsync();
+            return Environment.ExitCode;
+        }
+        else
+        {
+            try
             {
-                await CreateWebHostBuilder(args).Build().RunAsync();
-                return Environment.ExitCode;
+                IServiceScope? scope = null;
+                int result = await CreateCliHostBuilder().RunCommandLineApplicationAsync<KorgaCommand>(args, app =>
+                {
+                    scope = app.CreateScope();
+                    app.Conventions.UseConstructorInjection(scope.ServiceProvider);
+                });
+                scope?.Dispose();
+                return result;
             }
-            else
+            catch (UnrecognizedCommandParsingException) // Host integration of v3.0.0 does not support disabling this exception
             {
-                try
-                {
-                    IServiceScope? scope = null;
-                    int result = await CreateCliHostBuilder().RunCommandLineApplicationAsync<KorgaCommand>(args, app =>
-                    {
-                        scope = app.CreateScope();
-                        app.Conventions.UseConstructorInjection(scope.ServiceProvider);
-                    });
-                    scope?.Dispose();
-                    return result;
-                }
-                catch (UnrecognizedCommandParsingException) // Host integration of v3.0.0 does not support disabling this exception
-                {
-                    return 1;
-                }
+                return 1;
             }
         }
-
-        private static IHostBuilder CreateWebHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStartup<Startup>();
-                });
-
-        private static IHostBuilder CreateCliHostBuilder() =>
-            Host.CreateDefaultBuilder()
-                .ConfigureAppConfiguration((context, config) =>
-                {
-                    if (context.HostingEnvironment.IsDevelopment())
-                    {
-                        config.AddUserSecrets<Program>();
-                    }
-                })
-                .ConfigureServices((context, services) =>
-                {
-                    services.ConfigureKorga(context.Configuration);
-                    services.AddSingleton<LdapService>();
-                    services.AddDbContext<DatabaseContext>();
-                });
     }
+
+    private static IHostBuilder CreateWebHostBuilder(string[] args) =>
+        Host.CreateDefaultBuilder(args)
+            .ConfigureWebHostDefaults(webBuilder =>
+            {
+                webBuilder.UseStartup<Startup>();
+            });
+
+    private static IHostBuilder CreateCliHostBuilder() =>
+        Host.CreateDefaultBuilder()
+            .ConfigureAppConfiguration((context, config) =>
+            {
+                if (context.HostingEnvironment.IsDevelopment())
+                {
+                    config.AddUserSecrets<Program>();
+                }
+            })
+            .ConfigureServices((context, services) =>
+            {
+                services.ConfigureKorga(context.Configuration);
+                services.AddSingleton<LdapService>();
+                services.AddMySqlDatabase();
+            });
 }
