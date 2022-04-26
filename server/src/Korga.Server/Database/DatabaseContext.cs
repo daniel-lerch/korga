3ef1d736049244550a5ca4eb8a8ecc545430b91d
Daniel Lerch <36048059+daniel-lerch@users.noreply.github.com>
2022-04-26 23:35:57 +0200

    Dynamically choose EF Core provider

--- server/src/Korga.Server/Database/DatabaseContext.cs
+++ server/src/Korga.Server/Database/DatabaseContext.cs
@@ -1,60 +1,41 @@
-﻿using Korga.Server.Configuration;
-using Korga.Server.Database.Entities;
+﻿using Korga.Server.Database.Entities;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.Options;
 
-namespace Korga.Server.Database
+namespace Korga.Server.Database;
+
+public sealed partial class DatabaseContext : DbContext
 {
-    public sealed partial class DatabaseContext : DbContext
+    public DbSet<Event> Events => Set<Event>();
+    public DbSet<EventProgram> EventPrograms => Set<EventProgram>();
+    public DbSet<EventParticipant> EventParticipants => Set<EventParticipant>();
+    public DbSet<EventRegistration> EventRegistrations => Set<EventRegistration>();
+
+    public DatabaseContext(DbContextOptions<DatabaseContext> options) : base(options) { }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
-        private readonly IOptions<DatabaseOptions> options;
-        private readonly ILoggerFactory loggerFactory;
+        base.OnModelCreating(modelBuilder);
 
-        public DbSet<Event> Events => Set<Event>();
-        public DbSet<EventProgram> EventPrograms => Set<EventProgram>();
-        public DbSet<EventParticipant> EventParticipants => Set<EventParticipant>();
-        public DbSet<EventRegistration> EventRegistrations => Set<EventRegistration>();
+        var @event = modelBuilder.Entity<Event>();
+        @event.HasKey(e => e.Id);
+        @event.Property(e => e.Start).HasPrecision(0);
+        @event.Property(e => e.End).HasPrecision(0);
+        @event.Property(e => e.RegistrationStart).HasPrecision(0);
+        @event.Property(e => e.RegistrationDeadline).HasPrecision(0);
 
-        public DatabaseContext(IOptions<DatabaseOptions> options, ILoggerFactory loggerFactory)
-        {
-            this.options = options;
-            this.loggerFactory = loggerFactory;
-        }
+        var program = modelBuilder.Entity<EventProgram>();
+        program.HasKey(p => p.Id);
+        program.HasOne(p => p.Event).WithMany().HasForeignKey(p => p.EventId);
 
-        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-        {
-            base.OnConfiguring(optionsBuilder);
+        var registration = modelBuilder.Entity<EventRegistration>();
+        registration.HasKey(r => r.Id);
+        registration.HasOne(r => r.Event).WithMany().HasForeignKey(r => r.EventId);
+        registration.HasAlternateKey(r => r.Token);
+        registration.Property(r => r.Token).HasConversion<string>();
 
-            optionsBuilder.UseLoggerFactory(loggerFactory);
-            optionsBuilder.UseMySql(options.Value.ConnectionString, ServerVersion.AutoDetect(options.Value.ConnectionString));
-        }
-
-        protected override void OnModelCreating(ModelBuilder modelBuilder)
-        {
-            base.OnModelCreating(modelBuilder);
-
-            var @event = modelBuilder.Entity<Event>();
-            @event.HasKey(e => e.Id);
-            @event.Property(e => e.Start).HasPrecision(0);
-            @event.Property(e => e.End).HasPrecision(0);
-            @event.Property(e => e.RegistrationStart).HasPrecision(0);
-            @event.Property(e => e.RegistrationDeadline).HasPrecision(0);
-
-            var program = modelBuilder.Entity<EventProgram>();
-            program.HasKey(p => p.Id);
-            program.HasOne(p => p.Event).WithMany().HasForeignKey(p => p.EventId);
-
-            var registration = modelBuilder.Entity<EventRegistration>();
-            registration.HasKey(r => r.Id);
-            registration.HasOne(r => r.Event).WithMany().HasForeignKey(r => r.EventId);
-            registration.HasAlternateKey(r => r.Token);
-            registration.Property(r => r.Token).HasConversion<string>();
-
-            var participant = modelBuilder.Entity<EventParticipant>();
-            participant.HasKey(p => p.Id);
-            participant.HasOne(p => p.Program).WithMany(p => p.Participants).HasForeignKey(p => p.ProgramId);
-            participant.HasOne(p => p.Registration).WithMany(r => r.Participants).HasForeignKey(p => p.RegistrationId);
-        }
+        var participant = modelBuilder.Entity<EventParticipant>();
+        participant.HasKey(p => p.Id);
+        participant.HasOne(p => p.Program).WithMany(p => p.Participants).HasForeignKey(p => p.ProgramId);
+        participant.HasOne(p => p.Registration).WithMany(r => r.Participants).HasForeignKey(p => p.RegistrationId);
     }
 }
