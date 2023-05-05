a19bd8d19d3c0dea844d3be66a8946f652aeb2cb
Daniel Lerch <36048059+daniel-lerch@users.noreply.github.com>
2023-05-05 15:09:24 +0200

    Sync departments from ChurchTools

--- server/src/Korga.Core/DatabaseContext.cs
+++ server/src/Korga.Core/DatabaseContext.cs
@@ -16,126 +16,137 @@ namespace Korga;
 /// </remarks>
 public sealed class DatabaseContext : DbContext
 {
-	public DbSet<Event> Events => Set<Event>();
-	public DbSet<EventProgram> EventPrograms => Set<EventProgram>();
-	public DbSet<EventParticipant> EventParticipants => Set<EventParticipant>();
+    public DbSet<Event> Events => Set<Event>();
+    public DbSet<EventProgram> EventPrograms => Set<EventProgram>();
+    public DbSet<EventParticipant> EventParticipants => Set<EventParticipant>();
 
-	public DbSet<Person> People => Set<Person>();
-	public DbSet<Group> Groups => Set<Group>();
-	public DbSet<GroupMember> GroupMembers => Set<GroupMember>();
-	public DbSet<GroupRole> GroupRoles => Set<GroupRole>();
-	public DbSet<GroupType> GroupTypes => Set<GroupType>();
-	public DbSet<Status> Status => Set<Status>();
+    public DbSet<Person> People => Set<Person>();
+    public DbSet<Group> Groups => Set<Group>();
+    public DbSet<GroupMember> GroupMembers => Set<GroupMember>();
+    public DbSet<GroupRole> GroupRoles => Set<GroupRole>();
+    public DbSet<GroupType> GroupTypes => Set<GroupType>();
+    public DbSet<Department> Departments => Set<Department>();
+    public DbSet<DepartmentMember> DepartmentMembers => Set<DepartmentMember>();
+    public DbSet<Status> Status => Set<Status>();
 
-	public DbSet<InboxEmail> InboxEmails => Set<InboxEmail>();
-	public DbSet<DistributionList> DistributionLists => Set<DistributionList>();
-	public DbSet<PersonFilter> PersonFilters => Set<PersonFilter>();
+    public DbSet<InboxEmail> InboxEmails => Set<InboxEmail>();
+    public DbSet<DistributionList> DistributionLists => Set<DistributionList>();
+    public DbSet<PersonFilter> PersonFilters => Set<PersonFilter>();
 
-	public DbSet<OutboxEmail> OutboxEmails => Set<OutboxEmail>();
+    public DbSet<OutboxEmail> OutboxEmails => Set<OutboxEmail>();
 
-	public DbSet<PasswordReset> PasswordResets => Set<PasswordReset>();
+    public DbSet<PasswordReset> PasswordResets => Set<PasswordReset>();
 
 
-	public DatabaseContext(DbContextOptions<DatabaseContext> options) : base(options) { }
+    public DatabaseContext(DbContextOptions<DatabaseContext> options) : base(options) { }
 
-	protected override void OnModelCreating(ModelBuilder modelBuilder)
-	{
-		base.OnModelCreating(modelBuilder);
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
 
-		CreateEvents(modelBuilder);
+        CreateEvents(modelBuilder);
 
-		CreateChurchTools(modelBuilder);
+        CreateChurchTools(modelBuilder);
 
-		CreateEmailRelay(modelBuilder);
+        CreateEmailRelay(modelBuilder);
 
-		CreateEmailDelivery(modelBuilder);
+        CreateEmailDelivery(modelBuilder);
 
-		CreateLdap(modelBuilder);
-	}
+        CreateLdap(modelBuilder);
+    }
 
-	private void CreateEvents(ModelBuilder modelBuilder)
-	{
-		var @event = modelBuilder.Entity<Event>();
-		@event.HasKey(e => e.Id);
+    private void CreateEvents(ModelBuilder modelBuilder)
+    {
+        var @event = modelBuilder.Entity<Event>();
+        @event.HasKey(e => e.Id);
 
-		var program = modelBuilder.Entity<EventProgram>();
-		program.HasKey(p => p.Id);
-		program.HasOne(p => p.Event).WithMany().HasForeignKey(p => p.EventId);
+        var program = modelBuilder.Entity<EventProgram>();
+        program.HasKey(p => p.Id);
+        program.HasOne(p => p.Event).WithMany().HasForeignKey(p => p.EventId);
 
-		var participant = modelBuilder.Entity<EventParticipant>();
-		participant.HasKey(p => p.Id);
-		participant.HasOne(p => p.Program).WithMany(p => p.Participants).HasForeignKey(p => p.ProgramId);
-	}
+        var participant = modelBuilder.Entity<EventParticipant>();
+        participant.HasKey(p => p.Id);
+        participant.HasOne(p => p.Program).WithMany(p => p.Participants).HasForeignKey(p => p.ProgramId);
+    }
 
-	private void CreateChurchTools(ModelBuilder modelBuilder)
-	{
-		var person = modelBuilder.Entity<Person>();
-		person.HasKey(p => p.Id);
-		person.HasOne(p => p.Status).WithMany().HasForeignKey(p => p.StatusId);
-		person.Property(p => p.Id).ValueGeneratedNever();
+    private void CreateChurchTools(ModelBuilder modelBuilder)
+    {
+        var person = modelBuilder.Entity<Person>();
+        person.HasKey(p => p.Id);
+        person.HasOne(p => p.Status).WithMany().HasForeignKey(p => p.StatusId);
+        person.Property(p => p.Id).ValueGeneratedNever();
 
-		var group = modelBuilder.Entity<Group>();
-		group.HasKey(x => x.Id);
-		group.HasOne(x => x.GroupType).WithMany().HasForeignKey(x => x.GroupTypeId);
-		group.Property(x => x.Id).ValueGeneratedNever();
+        var group = modelBuilder.Entity<Group>();
+        group.HasKey(x => x.Id);
+        group.HasOne(x => x.GroupType).WithMany().HasForeignKey(x => x.GroupTypeId);
+        group.Property(x => x.Id).ValueGeneratedNever();
 
-		var groupMember = modelBuilder.Entity<GroupMember>();
-		groupMember.HasKey(gm => new { gm.PersonId, gm.GroupId });
-		groupMember.HasOne(gm => gm.Person).WithMany().HasForeignKey(gm => gm.PersonId);
-		groupMember.HasOne(gm => gm.Group).WithMany().HasForeignKey(gm => gm.GroupId);
-		groupMember.HasOne(gm => gm.GroupRole).WithMany().HasForeignKey(gm => gm.GroupRoleId);
+        var groupMember = modelBuilder.Entity<GroupMember>();
+        groupMember.HasKey(gm => new { gm.PersonId, gm.GroupId });
+        groupMember.HasOne(gm => gm.Person).WithMany().HasForeignKey(gm => gm.PersonId);
+        groupMember.HasOne(gm => gm.Group).WithMany().HasForeignKey(gm => gm.GroupId);
+        groupMember.HasOne(gm => gm.GroupRole).WithMany().HasForeignKey(gm => gm.GroupRoleId);
 
-		var groupType = modelBuilder.Entity<GroupType>();
-		groupType.HasKey(x => x.Id);
-		groupType.Property(x => x.Id).ValueGeneratedNever();
+        var groupType = modelBuilder.Entity<GroupType>();
+        groupType.HasKey(x => x.Id);
+        groupType.Property(x => x.Id).ValueGeneratedNever();
 
-		var groupRole = modelBuilder.Entity<GroupRole>();
-		groupRole.HasKey(x => x.Id);
-		groupRole.HasOne(x => x.GroupType).WithMany().HasForeignKey(x => x.GroupTypeId);
-		groupRole.Property(x => x.Id).ValueGeneratedNever();
+        var groupRole = modelBuilder.Entity<GroupRole>();
+        groupRole.HasKey(x => x.Id);
+        groupRole.HasOne(x => x.GroupType).WithMany().HasForeignKey(x => x.GroupTypeId);
+        groupRole.Property(x => x.Id).ValueGeneratedNever();
 
-		var status = modelBuilder.Entity<Status>();
-		status.HasKey(x => x.Id);
-		status.Property(x => x.Id).ValueGeneratedNever();
-	}
+        var department = modelBuilder.Entity<Department>();
+        department.HasKey(x => x.Id);
+        department.Property(x => x.Id).ValueGeneratedNever();
 
-	private void CreateEmailRelay(ModelBuilder modelBuilder)
-	{
-		var inboxEmail = modelBuilder.Entity<InboxEmail>();
-		inboxEmail.HasKey(e => e.Id);
-		inboxEmail.HasOne(e => e.DistributionList).WithMany().HasForeignKey(e => e.DistributionListId);
-		inboxEmail.Property(e => e.DownloadTime).HasDefaultValueSql("CURRENT_TIMESTAMP(6)");
+        var departmentMember = modelBuilder.Entity<DepartmentMember>();
+        departmentMember.HasKey(x => new { x.PersonId, x.DepartmentId });
+        departmentMember.HasOne(x => x.Person).WithMany(p => p.Departments).HasForeignKey(x => x.PersonId);
+        departmentMember.HasOne(x => x.Department).WithMany().HasForeignKey(x => x.DepartmentId);
 
-		var distributionList = modelBuilder.Entity<DistributionList>();
-		distributionList.HasKey(dl => dl.Id);
-		distributionList.HasAlternateKey(dl => dl.Alias);
-		distributionList.Property(dl => dl.Flags).HasConversion<int>();
+        var status = modelBuilder.Entity<Status>();
+        status.HasKey(x => x.Id);
+        status.Property(x => x.Id).ValueGeneratedNever();
+    }
 
-		var personFilter = modelBuilder.Entity<PersonFilter>();
-		personFilter.HasKey(f => f.Id);
-		personFilter.HasOne(f => f.DistributionList).WithMany(dl => dl.Filters).HasForeignKey(f => f.DistributionListId);
+    private void CreateEmailRelay(ModelBuilder modelBuilder)
+    {
+        var inboxEmail = modelBuilder.Entity<InboxEmail>();
+        inboxEmail.HasKey(e => e.Id);
+        inboxEmail.HasOne(e => e.DistributionList).WithMany().HasForeignKey(e => e.DistributionListId);
+        inboxEmail.Property(e => e.DownloadTime).HasDefaultValueSql("CURRENT_TIMESTAMP(6)");
 
-		var groupFilter = modelBuilder.Entity<GroupFilter>();
-		groupFilter.HasOne(f => f.Group).WithMany().HasForeignKey(f => f.GroupId);
-		groupFilter.HasOne(f => f.GroupRole).WithMany().HasForeignKey(f => f.GroupRoleId);
+        var distributionList = modelBuilder.Entity<DistributionList>();
+        distributionList.HasKey(dl => dl.Id);
+        distributionList.HasAlternateKey(dl => dl.Alias);
+        distributionList.Property(dl => dl.Flags).HasConversion<int>();
 
-		var statusFilter = modelBuilder.Entity<StatusFilter>();
-		statusFilter.HasOne(s => s.Status).WithMany().HasForeignKey(s => s.StatusId);
+        var personFilter = modelBuilder.Entity<PersonFilter>();
+        personFilter.HasKey(f => f.Id);
+        personFilter.HasOne(f => f.DistributionList).WithMany(dl => dl.Filters).HasForeignKey(f => f.DistributionListId);
 
-		var singlePerson = modelBuilder.Entity<SinglePerson>();
-		singlePerson.HasOne(p => p.Person).WithMany().HasForeignKey(p => p.PersonId);
-	}
+        var groupFilter = modelBuilder.Entity<GroupFilter>();
+        groupFilter.HasOne(f => f.Group).WithMany().HasForeignKey(f => f.GroupId);
+        groupFilter.HasOne(f => f.GroupRole).WithMany().HasForeignKey(f => f.GroupRoleId);
 
-	private void CreateEmailDelivery(ModelBuilder modelBuilder)
-	{
+        var statusFilter = modelBuilder.Entity<StatusFilter>();
+        statusFilter.HasOne(s => s.Status).WithMany().HasForeignKey(s => s.StatusId);
+
+        var singlePerson = modelBuilder.Entity<SinglePerson>();
+        singlePerson.HasOne(p => p.Person).WithMany().HasForeignKey(p => p.PersonId);
+    }
+
+    private void CreateEmailDelivery(ModelBuilder modelBuilder)
+    {
         var outboxEmail = modelBuilder.Entity<OutboxEmail>();
         outboxEmail.HasKey(e => e.Id);
         outboxEmail.HasOne(e => e.InboxEmail).WithMany(e => e.Recipients).HasForeignKey(e => e.InboxEmailId);
     }
 
     private void CreateLdap(ModelBuilder modelBuilder)
-	{
-		var passwordReset = modelBuilder.Entity<PasswordReset>();
-		passwordReset.HasKey(r => r.Token);
-	}
+    {
+        var passwordReset = modelBuilder.Entity<PasswordReset>();
+        passwordReset.HasKey(r => r.Token);
+    }
 }
