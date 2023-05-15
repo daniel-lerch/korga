d257b8d564d91c591dad4379b3617c4a0dfe7cf5
Daniel Lerch <36048059+daniel-lerch@users.noreply.github.com>
2023-05-16 00:01:33 +0200

    [WIP] Create custom JWT with ChurchTools login

--- server/src/Korga.Server/Controllers/IdentityController.cs
+++ server/src/Korga.Server/Controllers/IdentityController.cs
@@ -2,9 +2,13 @@
 using Korga.Server.Models.Json;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
 using System;
+using System.Globalization;
+using System.IdentityModel.Tokens.Jwt;
 using System.Net.Http;
 using System.Net.Http.Json;
+using System.Security.Claims;
 using System.Threading.Tasks;
 
 namespace Korga.Server.Controllers;
@@ -26,6 +30,25 @@ public class IdentityController : ControllerBase
 
         var response = await httpClient.PostAsJsonAsync("/api/login", new { request.Username, request.Password, RememberMe = false });
 
-        return StatusCode(response.IsSuccessStatusCode ? 204 : 400);
+        if (!response.IsSuccessStatusCode) return BadRequest();
+
+        var claims = new Claim[]
+        {
+            new(JwtRegisteredClaimNames.Sub, ""),
+            new(JwtRegisteredClaimNames.Iat, DateTime.UtcNow.ToString(CultureInfo.InvariantCulture)),
+            new(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString())
+        };
+
+        var signingCredentials = new SigningCredentials(new SymmetricSecurityKey(Array.Empty<byte>()), SecurityAlgorithms.HmacSha256);
+
+        var token = new JwtSecurityToken(
+            issuer: "",
+            audience: "",
+            claims,
+            notBefore: null,
+            expires: DateTime.UtcNow.AddDays(1),
+            signingCredentials);
+
+        return Ok(new JwtSecurityTokenHandler().WriteToken(token));
     }
 }
