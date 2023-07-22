6d1ecd0f3ecd9c5bd3f5a86372c7913f4a5f601a
Daniel Lerch <36048059+daniel-lerch@users.noreply.github.com>
2023-07-22 19:10:34 +0200

    Migrate database on startup (enabled by default)

--- server/src/Korga.Server/Configuration/DatabaseOptions.cs
+++ server/src/Korga.Server/Configuration/DatabaseOptions.cs
@@ -6,4 +6,5 @@ public class DatabaseOptions
 {
     // Validation ensures that required values cannot be null
     [Required] public string ConnectionString { get; set; } = null!;
+    public bool MigrateOnStartup { get; set; }
 }
