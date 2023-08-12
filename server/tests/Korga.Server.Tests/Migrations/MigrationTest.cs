0abe941cd5cc8d3b828b086a5785575aafa86f37
Daniel Lerch <36048059+daniel-lerch@users.noreply.github.com>
2023-08-12 06:32:19 +0200

    Add unit tests for database migrations (#31)

---
+++ server/tests/Korga.Server.Tests/Migrations/MigrationTest.cs
@@ -0,0 +1,83 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using MySqlConnector;
+using System;
+
+namespace Korga.Server.Tests.Migrations;
+
+public abstract class MigrationTest : IDisposable
+{
+    protected readonly string databaseName;
+    protected readonly string shortConnectionString;
+    protected readonly string connectionString;
+    protected readonly MySqlConnection connection;
+    protected readonly ServiceProvider serviceProvider;
+    protected readonly IServiceScope serviceScope;
+    protected readonly DatabaseContext databaseContext;
+
+    public MigrationTest(string testName)
+    {
+        databaseName = $"Korga_{testName}";
+        shortConnectionString = "Server=localhost;Port=3306;User=root;Password=root;";
+        connectionString = $"Server=localhost;Port=3306;Database={databaseName};User=root;Password=root;";
+
+        // Create test database
+        connection = new(shortConnectionString);
+        connection.Open();
+        CreateEmptyDatabase(connection, databaseName);
+        connection.ChangeDatabase(databaseName);
+
+        // Create DatabaseContext for migrations
+        var serviceCollection = new ServiceCollection()
+            .AddDbContext<DatabaseContext>(optionsBuilder =>
+            {
+                optionsBuilder.UseMySql(
+                    connectionString,
+                    ServerVersion.AutoDetect(shortConnectionString),
+                    builder =>
+                    {
+                        builder.MigrationsAssembly($"{nameof(Korga)}.{nameof(Server)}");
+                        builder.EnableRetryOnFailure();
+                    });
+            });
+        ConfigureServices(serviceCollection);
+        serviceProvider = serviceCollection.BuildServiceProvider();
+
+        serviceScope = serviceProvider.CreateScope();
+        databaseContext = serviceScope.ServiceProvider.GetRequiredService<DatabaseContext>();
+    }
+
+    public void Dispose()
+    {
+        serviceScope.Dispose();
+        serviceProvider.Dispose();
+
+        DropDatabase(connection, databaseName);
+
+        connection.Close();
+        connection.Dispose();
+    }
+
+    protected virtual void ConfigureServices(IServiceCollection services) { }
+
+    private static void CreateEmptyDatabase(MySqlConnection connection, string databaseName)
+    {
+        using (MySqlCommand command = connection.CreateCommand())
+        {
+            command.CommandText = $"DROP DATABASE IF EXISTS {databaseName}";
+            command.ExecuteNonQuery();
+        }
+        using (MySqlCommand command = connection.CreateCommand())
+        {
+            command.CommandText = $"CREATE DATABASE {databaseName}";
+            command.ExecuteNonQuery();
+        }
+    }
+
+    private static void DropDatabase(MySqlConnection connection, string databaseName)
+    {
+        using MySqlCommand command = connection.CreateCommand();
+        command.CommandText = $"DROP DATABASE {databaseName}";
+        command.ExecuteNonQuery();
+    }
+}
