de77ce7a9c2f7722b9eaa6830bd784fe88010c0b
Daniel Lerch <36048059+daniel-lerch@users.noreply.github.com>
2024-05-23 22:49:10 +0200

    Implement upgrade path of database migration

--- server/Korga/Extensions/CollectionExtensions.cs
+++ server/Korga/Extensions/CollectionExtensions.cs
@@ -80,12 +80,4 @@ public static class CollectionExtensions
 			}
 		}
 	}
-
-    public static void AddRange<T>(this ICollection<T> collection, IEnumerable<T> values)
-    {
-        foreach (var item in values)
-        {
-            collection.Add(item);
-        }
-    }
 }
--- server/Korga/Migrations/20240523161352_PersonFilterList.Designer.cs
+++ server/Korga/Migrations/20240523161352_PersonFilterList.Designer.cs
@@ -4,6 +4,7 @@ using Korga;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Korga.Migrations
 {
     [DbContext(typeof(DatabaseContext))]
-    partial class DatabaseContextModelSnapshot : ModelSnapshot
+    [Migration("20240523161352_PersonFilterList")]
+    partial class PersonFilterList
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
