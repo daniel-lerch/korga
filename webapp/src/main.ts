c80dee2ff014f68b952cd1b0711ce4cf9876beae
Daniel Lerch <36048059+daniel-lerch@users.noreply.github.com>
2025-11-23 14:35:28 +0100

    Replace Bootstrap with Tailwind CSS 4.1 (#82)

--- webapp/src/main.ts
+++ webapp/src/main.ts
@@ -2,8 +2,6 @@ import { createPinia } from "pinia"
 import { createApp } from "vue"
 import App from "./App.vue"
 import router from "./router"
-import "bootstrap"
-import "./custom_styles.scss"
 
 const app = createApp(App)
 app.use(router)
